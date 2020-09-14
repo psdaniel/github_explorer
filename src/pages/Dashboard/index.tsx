02e927113bbe7f8def15796679945db12b4cd5d6
Daniel Souza <daniel.n10@hotmail.com>
2020-09-14 13:39:22 -0300

    conexão com a api do github

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,33 +1,62 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
 
 import { Title, Form, Repositories } from './styles';
 
+interface Repository {
+    full_name: string;
+    description: string;
+    owner: {
+        login: string;
+        avatar_url: string;
+    };
+}
+
 const Dashboard: React.FC = () => {
+    const [newRepo, setNewRepo] = useState('');
+    const [repositories, setRepositories] = useState<Repository[]>([]);
+
+    async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
+        event.preventDefault();
+
+        const response = await api.get<Repository>(`repos/${newRepo}`);
+
+        const repository = response.data;
+
+        setRepositories([...repositories, repository]);
+        setNewRepo('');
+    }
+
+
     return (
         <>
             <img src={logoImg} alt="Github Explorer" />
             <Title>Explore repositórios no Github</Title>
 
-            <Form>
-                <input placeholder="Digite aqui o nome do repositório" />
+            <Form onSubmit={handleAddRepository}>
+                <input
+                value={newRepo}
+                onChange={(e) => setNewRepo(e.target.value)}
+                placeholder="Digite aqui o nome do repositório" />
                 <button type="submit">Pesquisar</button>
             </Form>
 
             <Repositories>
-                <a href="teste">
-                    <img src="https://avatars2.githubusercontent.com/u/67653200?s=460&u=cebdbd834f835b444ccfc339e972a3d5dfa93cf3&v=4"
-                    alt="Daniel Souza"
+                {repositories.map(repository => (
+                <a key={repository.full_name} href="teste">
+                    <img src={repository.owner.avatar_url}
+                    alt={repository.owner.login}
                     />
                     <div>
-                        <strong>rocketseat/unform</strong>
-                        <p>Um repositório maneiro aí</p>
+                        <strong>{repository.full_name}</strong>
+                        <p>{repository.description}</p>
                     </div>
 
                     <FiChevronRight size={20} />
-                </a>
+                </a>))}
             </Repositories>
         </>
     );
