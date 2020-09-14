591854e73c491027940a942e28db224df2ee3adc
Daniel Souza <daniel.n10@hotmail.com>
2020-09-14 11:18:15 -0300

    finalização do estilo da dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-    return <Title>Explore repositórios no Github</Title>;
+    return (
+        <>
+            <img src={logoImg} alt="Github Explorer" />
+            <Title>Explore repositórios no Github</Title>
+
+            <Form>
+                <input placeholder="Digite aqui o nome do repositório" />
+                <button type="submit">Pesquisar</button>
+            </Form>
+
+            <Repositories>
+                <a href="teste">
+                    <img src="https://avatars2.githubusercontent.com/u/67653200?s=460&u=cebdbd834f835b444ccfc339e972a3d5dfa93cf3&v=4"
+                    alt="Daniel Souza"
+                    />
+                    <div>
+                        <strong>rocketseat/unform</strong>
+                        <p>Um repositório maneiro aí</p>
+                    </div>
+
+                    <FiChevronRight size={20} />
+                </a>
+            </Repositories>
+        </>
+    );
 };
 
 export default Dashboard;
